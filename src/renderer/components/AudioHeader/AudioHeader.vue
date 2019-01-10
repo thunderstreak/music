9ae78940d1e919cb63ec9475fbe5655a7234447d
thunderstreak <821470904@qq.com>
2019-01-10 14:45:27 +0800

    optimizing search song lists

--- src/renderer/components/AudioHeader/AudioHeader.vue
+++ src/renderer/components/AudioHeader/AudioHeader.vue
@@ -9,12 +9,12 @@
                        @change.stop="searchMusics('change')"
                        @blur.stop="searchMusics('blur')"
                        @input.stop="searchMusics('input')"
+                       @focus.stop="searchMusics('focus')"
                        placeholder="搜索歌曲/歌手">
                 <transition name="slide-fade">
                     <ul class="hero-gesture-search-res" v-show="isShowList">
                         <li class="res-list" v-for="item in searchList" @click.stop="selectPlaySong(item)">
                             {{item.singer.length !== 0 ? item.singer[0].name : item.singername}}-{{item.songname}}
-                            <!--{{item.fsinger}}-{{item.fsong}}-->
                         </li>
                     </ul>
                 </transition>
@@ -84,30 +84,29 @@ export default {
         // 搜索音乐
         searchMusics(eventType){
             console.log(eventType);
-            if(eventType === 'blur'){
-                setTimeout(()=>{
-                    this.isShowList = false;
-                },200);
-
-                if(this.searchVal === ''){
-                    return
-                }
-
-                return false;
-            }else if(eventType === 'input'){
-                if(this.searchVal === ''){
-                    return
-                }
-                // this.$tool.debounce(this.searchApi(),500);
-                this.searchApi();
-            }else if(eventType === 'change'){
-                if(this.searchVal === ''){
-                    this.isShowList = false;
-                    return
-                }
-                this.searchApi();//搜索
+            if(this.searchVal === ''){
+                this.isShowList = false;
+                return
             }
 
+            switch (eventType) {
+                case 'blur':
+                    this.isShowList = false;
+                    return false;
+                case 'focus':
+                    if(this.searchList.length){
+                        this.isShowList = true;
+                    }
+                    return false;
+                case 'input':
+                    this.searchApi();
+                    return false;
+                case 'change':
+                    this.searchApi();
+                    return false;
+                default:
+                    return false;
+            }
         },
 
         // 选中播放歌曲
@@ -140,12 +139,12 @@ export default {
     transition: all .25s ease;
 }
 .slide-fade-leave-active{
-    transition: all .20s ease;
+    transition: all .25s;
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active for below version 2.1.8 */ {
-    transform:scaleX(0.5);
-    /*transform: translateY(10px);*/
+    /*transform:scaleX(0.5);*/
+    transform: translateY(10px);
     opacity: 0;
 }
 </style>
